104b21e0a5079c59fa53be39caffb74fcf24a4fc
jeffepok <56881629+jeffepok@users.noreply.github.com>
2020-10-23 17:03:53 +0000

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+def scrape():
+    source = requests.get('http://lyrics.ghospel.com/akan-songs/').text
+    soup = BeautifulSoup(source, 'lxml')
+    with open('lyrics_scrape.csv', 'w', newline='', encoding="utf-8") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['song_title', 'lyrics'])
+        links = soup.find('div', 'p', 'title', class_ = 'entry-content clearfix')
+        for link in links.find_all('a'):
+            lyrics_page = requests.get(link.get('href')).text
+            lyrics_soup = BeautifulSoup(lyrics_page, 'lxml')
+            lyrics = lyrics_soup.find('div', class_ = 'entry-content clearfix').text
+            song_title = lyrics_soup.title.text
+            spamwriter.writerow([song_title, lyrics])
+if __name__ == '__main__':
+    scrape()
