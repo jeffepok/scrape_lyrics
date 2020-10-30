b7be6dd5bb61da65669ad68525cb5340b7764187
jeffepok <56881629+jeffepok@users.noreply.github.com>
2020-10-30 05:05:31 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,18 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
+import pandas as pd
 def scrape():
     source = requests.get('http://lyrics.ghospel.com/akan-songs/').text
     soup = BeautifulSoup(source, 'lxml')
-    with open('lyrics_scrape.csv', 'w', newline='', encoding="utf-8") as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',', lineterminator = '+++')
-        spamwriter.writerow(['song_title', 'lyrics'])
-        links = soup.find('div', 'p', 'title', class_ = 'entry-content clearfix')
-        for link in links.find_all('a'):
-            lyrics_page = requests.get(link.get('href')).text
-            lyrics_soup = BeautifulSoup(lyrics_page, 'lxml')
-            lyrics = lyrics_soup.find('div', class_ = 'entry-content clearfix').text
-            song_title = lyrics_soup.title.text
-            spamwriter.writerow([song_title, lyrics])
+    links = soup.find('div', 'p', 'title', class_ = 'entry-content clearfix')
+    song_title_list = []
+    lyrics_list = []
+    for link in links.find_all('a'):
+        lyrics_page = requests.get(link.get('href')).text
+        lyrics_soup = BeautifulSoup(lyrics_page, 'lxml')
+        lyrics = lyrics_soup.find('div', class_ = 'entry-content clearfix').text
+        song_title_list.append(lyrics_soup.title.text)
+        lyrics_list.append(lyrics)
+    all_lyrics = {
+    'title': song_title_list,
+    'lyrics': lyrics_list
+    }
+    df = pd.DataFrame(all_lyrics)
+    df.to_csv('scrape.csv')
 if __name__ == '__main__':
     scrape()
